661e8f8b525534d21084b714158093e28a024ad7
Daniel Tan <dtch1997@users.noreply.github.com>
2023-06-04 10:53:45 +0100

    Add docstring

--- llm_curriculum/envs/wrappers/multi_task_env_wrapper.py
+++ llm_curriculum/envs/wrappers/multi_task_env_wrapper.py
@@ -4,6 +4,28 @@ import gym as gym_old
 
 
 class MTEnvWrapper(gym_old.Wrapper):
+    """
+    A multi-task environment wrapper that modifies the observation space of an underlying Gym environment.
+
+    This wrapper modifies the observation space of the underlying environment by adding a task observation
+    component to the original observation. The task observation component represents the task index.
+
+    Args:
+        env (gym.Env): The underlying Gym environment to be wrapped.
+        task_index (int): The task index associated with this instance of the environment wrapper.
+
+    Attributes:
+        observation_space (gym.spaces.Dict): The modified observation space of the environment. It is a dictionary
+            that contains two keys:
+                - 'env_obs': The original observation space of the underlying environment.
+                - 'task_obs': A Box space representing the task index.
+
+    Methods:
+        reset(): Resets the environment and returns the modified observation.
+        step(action): Performs a step in the environment and returns the modified observation, reward, done, and info.
+
+    """
+
     def __init__(self, env, task_index: int):
         super().__init__(env)
         self._env = env
@@ -17,15 +39,46 @@ class MTEnvWrapper(gym_old.Wrapper):
         )
 
     def reset(self):
-        # TODO: make sure correct task idx is set here...
+        """
+        Resets the environment and returns the modified observation.
+
+        Returns:
+            observation (dict): The modified observation containing both the environment observation and the
+                task observation component.
+
+        """
         obs = self._env.reset()
         return self.mod_observation(obs)
 
     def step(self, action):
+        """
+        Performs a step in the environment and returns the modified observation, reward, done, and info.
+
+        Args:
+            action: The action to take in the environment.
+
+        Returns:
+            observation (dict): The modified observation containing both the environment observation and the
+                task observation component.
+            reward (float): The reward obtained from the action.
+            done (bool): Indicates whether the episode is finished.
+            info (dict): Additional information about the step.
+
+        """
         obs, reward, done, info = self._env.step(action)
         return self.mod_observation(obs), reward, done, info
 
     def mod_observation(self, observation):
+        """
+        Modifies the observation by adding the task observation component.
+
+        Args:
+            observation: The original observation obtained from the underlying environment.
+
+        Returns:
+            modified_observation (dict): The modified observation containing both the environment observation and the
+                task observation component.
+
+        """
         task_obs = np.array([self._task_index])
-        # assert task_idx == info['active_task_idx'] # TODO
         return {"env_obs": observation["observation"], "task_obs": task_obs}
