d5848e7abc826f2db5208f34faf938e82cdc59f8
Daniel Tan <dtch1997@users.noreply.github.com>
2023-06-04 11:08:15 +0100

    Add skeleton for testing wrappers

---
+++ tests/envs/wrappers/test_multi_task_env_wrapper.py
@@ -0,0 +1,15 @@
+import numpy as np  # noqa
+import gymnasium as gym  # noqa
+import gym as gym_old  # noqa
+import unittest
+
+from llm_curriculum.envs.wrappers import MTEnvWrapper  # noqa
+
+
+class TestMTEnvWrapper(unittest.TestCase):
+    # TODO
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
