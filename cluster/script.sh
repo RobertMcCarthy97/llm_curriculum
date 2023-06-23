27e9cf12f4814a2b35d4c80749e8b173a7619818
dtch1997 <dtch1997@users.noreply.github.com>
2023-06-23 19:29:40 +0100

    Fix cluster script

--- cluster/script.sh
+++ cluster/script.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -l
+
 #$ -l h_rt=4:00:00      # Request 4 hours runtime
 #$ -l h_vmem=8G         # Request 8GB of memory
 #$ -pe smp 4            # Request 4 CPU cores
@@ -8,7 +9,7 @@ module load python/3.9.10
 module load default-modules
 
 .venv/bin/python llm_curriculum/learning/baselines/vanilla_rl.py \
-    --config llm_curriculum/learning/config/baseline.py \
+    --config llm_curriculum/learning/baselines/config/default.py \
     --config.use_her=True \
     --config.wandb.name='td3_her' \
     --config.wandb.track=True
