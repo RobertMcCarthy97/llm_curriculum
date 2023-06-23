7f57732b01e8f21b2336cc248299fec27bc0be3b
Daniel Tan <dtch1997@users.noreply.github.com>
2023-06-23 18:11:52 +0100

    Add workdir

--- cluster/script.sh
+++ cluster/script.sh
@@ -2,6 +2,7 @@
 #$ -l h_rt=4:00:00      # Request 4 hours runtime
 #$ -l h_vmem=8G         # Request 8GB of memory
 #$ -pe smp 4            # Request 4 CPU cores
+#$ -wd /home/ucabdc6/github/llm_curriculum
 
 module load python/3.9.10
 module load default-modules
