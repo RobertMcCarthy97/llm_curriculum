f91bc6df4e01f47c8b4c7db815d21ac9e98da68c
dtch1997 <dtch1997@users.noreply.github.com>
2023-06-26 10:12:06 +0100

    Fix incorrect path in TD3 script

--- cluster/td3.sh
+++ cluster/td3.sh
@@ -9,7 +9,7 @@ module load python/3.9.10
 module load default-modules
 
 .venv/bin/python llm_curriculum/learning/baselines/vanilla_rl.py \
-    --config llm_curriculum/learning/config/baseline.py \
+    --config llm_curriculum/learning/baselines/config/default.py \
     --config.use_her=False \
     --config.wandb.name='td3' \
     --config.wandb.track=True
