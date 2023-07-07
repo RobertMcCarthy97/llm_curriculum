44658e4bd41aa6deef536f0b5ec035bc61018409
Daniel Tan <dtch1997@users.noreply.github.com>
2023-07-07 15:55:59 +0100

    Add reward coding prompt

--- llm_curriculum/envs/minimal_minigrid/prompting/prompt.py
+++ llm_curriculum/envs/minimal_minigrid/prompting/prompt.py
@@ -1,7 +1,9 @@
 import llm_curriculum.envs.minimal_minigrid.envs
 import gymnasium as gym
 import json
+import re
 
+from typing import List
 from pathlib import Path
 from minigrid.wrappers import FullyObsWrapper
 from llm_curriculum.envs.minimal_minigrid.prompting.api import (
@@ -13,12 +15,34 @@ from llm_curriculum.envs.minimal_minigrid.description import (
     parse_field_of_view,
 )
 
-PROMPT_TEMPLATE_PATH = Path(__file__).parent / "decomposition_prompt_template.txt"
+DECOMPOSITION_PROMPT_TEMPLATE_PATH = (
+    Path(__file__).parent / "decomposition_prompt_template.txt"
+)
+REWARD_PROMPT_TEMPLATE_PATH = Path(__file__).parent / "reward_prompt_template.txt"
 
 
 def load_prompt_template(path: Path):
     with open(path) as f:
         return f.read()
+
+
+def is_objective(string: str) -> bool:
+    pattern = r"^\d+\.\s"  # This regular expression pattern matches one or more digits, followed by '. ', at the start of a string.
+    return bool(re.match(pattern, string))
+
+
+def parse_objectives(reply: str) -> List[str]:
+    objectives = []
+    lines = reply.split("\n")
+    pattern = r"^\d+\.\s"  # This regular expression pattern matches one or more digits, followed by '. ', at the start of a string.
+
+    for line in lines:
+        match = re.match(pattern, line)
+        if match is not None:
+            start, end = match.span()
+            match_len = end - start
+            objectives.append(line[match_len:])
+    return objectives
 
 
 if __name__ == "__main__":
@@ -36,7 +60,8 @@ if __name__ == "__main__":
     llm_obs_str = json.dumps(llm_obs)
     mission_str = obs["mission"]
 
-    prompt_template = load_prompt_template(PROMPT_TEMPLATE_PATH)
+    print(" ****** DECOMPOSITION PROMPT ****** ")
+    prompt_template = load_prompt_template(DECOMPOSITION_PROMPT_TEMPLATE_PATH)
     prompt = prompt_template.replace("${environment}", llm_obs_str).replace(
         "${mission}", mission_str
     )
@@ -44,13 +69,37 @@ if __name__ == "__main__":
     print(prompt)
 
     # Send the request
+    print(" ****** DECOMPOSITION RESPONSE ****** ")
     messages = [
         {"role": "system", "content": get_default_system_message()},
         {"role": "user", "content": prompt},
     ]
     response = chat_completion_request(messages)
     assistant_message = response.json()["choices"][0]["message"]
+    # Add response to history
+    messages.append(assistant_message)
+
     reply = assistant_message["content"]
     print(reply)
 
-    # TODO: Parse the response
+    # Parse the response
+    print(" ****** OBJECTIVES ****** ")
+    objectives = parse_objectives(reply)
+    print(objectives)
+
+    # Define reward coding prompt
+    print(" ****** REWARD PROMPT ****** ")
+    reward_prompt_template = load_prompt_template(REWARD_PROMPT_TEMPLATE_PATH)
+    reward_prompt = reward_prompt_template.replace("${state}", objectives[0])
+    print(reward_prompt)
+
+    # Send the request
+    print(" ****** REWARD RESPONSE ****** ")
+    messages.append({"role": "user", "content": reward_prompt_template})
+    response = chat_completion_request(messages)
+    assistant_message = response.json()["choices"][0]["message"]
+    # Add response to history
+    messages.append(assistant_message)
+
+    reply = assistant_message["content"]
+    print(reply)
