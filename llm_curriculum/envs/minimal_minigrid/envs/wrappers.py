4feba1e5e470085a310781c4c9d5a31a35486081
Daniel Tan <dtch1997@users.noreply.github.com>
2023-07-11 19:17:18 +0100

    Fix bug in wrapper

--- llm_curriculum/envs/minimal_minigrid/envs/wrappers.py
+++ llm_curriculum/envs/minimal_minigrid/envs/wrappers.py
@@ -103,17 +103,14 @@ class DecomposedRewardWrapper(gym.Wrapper):
         if self.all_subtasks_complete():
             return obs, orig_rew, term, trunc, info
 
+        sub_reward = orig_rew
         if self.enable_reward:
             # Shape the reward with intermediate objectives
             function_obs = self.get_reward_function_obs(self.env, full_obs)
             objective_completion = self.get_current_reward_function()(function_obs)
             if objective_completion:
                 self.current_objective_idx += 1
-                # Add back orig reward
-                # Ensure we don't diminish original reward signal
-                sub_reward = orig_rew + 1
-            else:
-                sub_reward = orig_rew
+                sub_reward += 1
 
         # Overwrite the mission with the current objective
         if self.enable_mission:
